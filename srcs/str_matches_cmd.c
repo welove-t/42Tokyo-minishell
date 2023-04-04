554e9abea3b09d5473babe8a029dcf3c102abdd3
love <tlove.tky@gmail.com>
2023-04-04 15:57:24 +0900

    tester対応（余計なprint除去）

--- srcs/str_matches_cmd.c
+++ srcs/str_matches_cmd.c
@@ -17,59 +17,71 @@ void line_matches_cmd(char *line)
 main.cで記述したグローバル変数を使用したいため
 extern宣言する
 */
-    char	**cmd_line = NULL;
-    t_token *token;
+	char	**cmd_line = NULL;
+	t_token *token;
 
-    token = tokenize(line);
-    expand(token);
-    cmd_line = token_list_to_array(token);
-    cmd_line[0] = get_cmd_array(ft_strtrim(cmd_line[0], " "));
-    if (strncmp("cd", line, 2) == 0)
-    {
-        printf("\x1b[32m文字列が\"cd\"と一致してます\x1b[0m\n");
-    }
-    else if (strncmp("echo", line, 4) == 0)
-    {
-        bi_echo(line);
-    }
-    else if (strncmp("env", line, 3) == 0)
-    {
-        printf("\x1b[32m文字列が\"env\"と一致してます\x1b[0m\n");
-    }
-    else if (strncmp("exit", line, 4) == 0)
-    {
-        printf("\x1b[32m文字列が\"exit\"と一致してます\x1b[0m\n");
-    }
-    else if (strncmp("export", line, 6) == 0)
-    {
-        printf("\x1b[32m文字列が\"export\"と一致してます\x1b[0m\n");
-    }
-    else if (strncmp("pwd", line, 3) == 0)
-    {
-        if (bi_pwd() == -1)
-            printf("\x1b[31m Error pwd \x1b[0m\n");
-    }
-    else if (strncmp("unset", line, 5) == 0)
-    {
-        printf("\x1b[32m文字列が\"unset\"と一致してます\x1b[0m\n");
-    }
-    /*
-    execveを使う
-	ビルトインコマンドを除くシステムコマンドは、execveでpath指定して実行する。
-	bult in cmd だけ、その処理を書いている関数を渡してあげる。
-    */
-   // TODO:システムコマンドだった場合
+	token = tokenize(line);
+	expand(token);
+	cmd_line = token_list_to_array(token);
+	cmd_line[0] = get_cmd_array(ft_strtrim(cmd_line[0], " "));
 
-   else if (cmd_line != NULL)
-   {
-       if (execve(cmd_line[0], cmd_line, environ) == -1)
-       {
-           printf("\x1b[31mError: execve()\x1b[0m\n");
-       }
-   }
-   else
-   {
-       printf("bash: %s: command not found\n",line);
-   }
-    return ;
+	if (cmd_line != NULL)
+	{
+		if (execve(cmd_line[0], cmd_line, environ) == -1)
+		{
+			// printf("\x1b[31mError: execve()\x1b[0m\n");
+		}
+	}
+	else
+		// printf("bash: %s: command not found\n",line);
+
+
+//     if (strncmp("cd", line, 2) == 0)
+//     {
+//         printf("\x1b[32m文字列が\"cd\"と一致してます\x1b[0m\n");
+//     }
+//     else if (strncmp("echo", line, 4) == 0)
+//     {
+//         bi_echo(line);
+//     }
+//     else if (strncmp("env", line, 3) == 0)
+//     {
+//         printf("\x1b[32m文字列が\"env\"と一致してます\x1b[0m\n");
+//     }
+//     else if (strncmp("exit", line, 4) == 0)
+//     {
+//         printf("\x1b[32m文字列が\"exit\"と一致してます\x1b[0m\n");
+//     }
+//     else if (strncmp("export", line, 6) == 0)
+//     {
+//         printf("\x1b[32m文字列が\"export\"と一致してます\x1b[0m\n");
+//     }
+//     else if (strncmp("pwd", line, 3) == 0)
+//     {
+//         if (bi_pwd() == -1)
+//             printf("\x1b[31m Error pwd \x1b[0m\n");
+//     }
+//     else if (strncmp("unset", line, 5) == 0)
+//     {
+//         printf("\x1b[32m文字列が\"unset\"と一致してます\x1b[0m\n");
+//     }
+//     /*
+//     execveを使う
+// 	ビルトインコマンドを除くシステムコマンドは、execveでpath指定して実行する。
+// 	bult in cmd だけ、その処理を書いている関数を渡してあげる。
+//     */
+//    // TODO:システムコマンドだった場合
+
+//    else if (cmd_line != NULL)
+//    {
+//        if (execve(cmd_line[0], cmd_line, environ) == -1)
+//        {
+//            printf("\x1b[31mError: execve()\x1b[0m\n");
+//        }
+//    }
+//    else
+//    {
+//        printf("bash: %s: command not found\n",line);
+//    }
+	return ;
 }
