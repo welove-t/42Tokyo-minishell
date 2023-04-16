6e2fc96bc57144ca0219153fc1183d904757c0db
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-04-16 13:14:05 +0900

    add:prompt状態のsignalの実装(playground)

--- srcs/builtin/env.c
+++ srcs/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 14:27:04 by susasaki          #+#    #+#             */
-/*   Updated: 2023/04/05 21:46:28 by susasaki         ###   ########.fr       */
+/*   Updated: 2023/04/15 15:28:44 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,8 @@ void bi_env(t_environ *environ)
 }
 
 
-
-// void bl_env(void)
-// {
-//     //環境変数environの値を直接変更したくないため
-//     char **tmp;
-    
-//     tmp = environ;
-//     //環境変数を1行ずつ出力
-//     while (*tmp != NULL)
-//     {
-//         printf("%s\n",*tmp);
-//         tmp++;
-//     }
-// }
-
 // int main(void)
 // {
 //     bl_env();
 //     return 0;
-// }
+// }N
