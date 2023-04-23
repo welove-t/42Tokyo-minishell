9dc40122deb789a0426d6dc85e34a2ed22c64ff0
love <tlove.tky@gmail.com>
2023-04-23 12:51:11 +0900

    tokenizer-error quoteのuncloseメッセージ内容の出力方法を変更

--- srcs/error_special.c
+++ srcs/error_special.c
@@ -6,25 +6,11 @@
 /*   By: terabu <terabu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 12:58:24 by terabu            #+#    #+#             */
-/*   Updated: 2023/04/21 11:11:19 by terabu           ###   ########.fr       */
+/*   Updated: 2023/04/23 12:18:28 by terabu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-#define ERROR_PREFIX "minishell: "
-
-void	tokenize_error(const char *location, char **rest, char *line)
-{
-	syntax_error = true;
-	perror_prefix();
-	dprintf(STDERR_FILENO, "syntax error near unexpected character \
-	`%c' in %s\n", *line, location);
-	// exit(1);
-	while (*line)
-		line++;
-	*rest = line;
-}
 
 void	parse_error(const char *location, t_token **rest, t_token *tok)
 {
--- srcs/error_tokenizer.c
+++ srcs/error_tokenizer.c
@@ -1,47 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error_general.c                                    :+:      :+:    :+:   */
+/*   error_tokenizer.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: terabu <terabu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 12:58:24 by terabu            #+#    #+#             */
-/*   Updated: 2023/04/23 12:18:34 by terabu           ###   ########.fr       */
+/*   Updated: 2023/04/23 12:47:36 by terabu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	perror_prefix(void)
+void	tokenize_error(const char *location, char **rest, char *line, int flg)
 {
-	dprintf(STDERR_FILENO, "%s", ERROR_PREFIX);
-}
-
-void	fatal_error(const char *msg)
-{
+	syntax_error = true;
 	perror_prefix();
-	dprintf(STDERR_FILENO, "Fatal Error: %s\n", msg);
-	exit(1);
+	if (flg < 0)
+		dprintf(STDERR_FILENO, "syntax error: %s\n", location);
+	else
+	{
+		dprintf(STDERR_FILENO, "syntax error near unexpected character \
+		`%c' in %s\n", *line, location);
+	}
+	while (*line)
+		line++;
+	*rest = line;
 }
-
-void	assert_error(const char *msg)
-{
-	perror_prefix();
-	dprintf(STDERR_FILENO, "Fatal Error: %s\n", msg);
-	exit(255);
-}
-
-void	err_exit(const char *location, const char *msg, int status)
-{
-	perror_prefix();
-	dprintf(STDERR_FILENO, "%s: %s\n", location, msg);
-	exit(status);
-}
-
-void	todo(const char *msg)
-{
-	perror_prefix();
-	dprintf(STDERR_FILENO, "TODO: %s\n", msg);
-	exit(255);
-}
-
