f69fc818c6584a8b2ad2f3731b385d777fa81761
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-04-29 17:48:35 +0900

    リファクタリング: search_bi_cmd.c

--- srcs/utils/search_bi_cmd.c
+++ srcs/utils/search_bi_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   search_bi_cmd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: terabu <terabu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:45:48 by susasaki          #+#    #+#             */
-/*   Updated: 2023/04/29 16:54:15 by terabu           ###   ########.fr       */
+/*   Updated: 2023/04/29 17:47:53 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,55 +56,54 @@ static int	search_bi_cmd_helper(int argc, char **argv, t_environ *environ)
 	return (flag);
 }
 
-int	get_args_cnt(t_token *token)
-{
-	int		cnt;
-	t_token	*tmp_token;
+// int	get_args_cnt(t_token *token)
+// {
+// 	int		cnt;
+// 	t_token	*tmp_token;
 
-	cnt = 0;
-	tmp_token = token;
-	while (tmp_token->next != NULL)
-	{
-		tmp_token = tmp_token->next;
-		cnt++;
-	}
-	return (cnt);
-}
+// 	cnt = 0;
+// 	tmp_token = token;
+// 	while (tmp_token->next != NULL)
+// 	{
+// 		tmp_token = tmp_token->next;
+// 		cnt++;
+// 	}
+// 	return (cnt);
+// }
 
-char	**string_to_array(int *argc, t_token *token)
-{
-	t_token	*tmp_token;
-	char	**argv;
-	int		i;
-	int		cnt_args;
+// char	**string_to_array(int *argc, t_token *token)
+// {
+// 	t_token	*tmp_token;
+// 	char	**argv;
+// 	int		i;
 
-	tmp_token = token;
-	if (tmp_token->word == NULL)
-		return (NULL);
-	cnt_args = get_args_cnt(tmp_token);
-	*argc = cnt_args;
-	argv = malloc(sizeof(char *) * (cnt_args + 1));
-	if (argv == NULL)
-		fatal_error("malloc error");
-	i = 0;
-	while (tmp_token->word != NULL)
-	{
-		argv[i] = tmp_token->word;
-		tmp_token = tmp_token->next;
-		i++;
-	}
-	argv[i] = NULL;
-	return (argv);
-}
+// 	tmp_token = token;
+// 	if (tmp_token->word == NULL)
+// 		return (NULL);
+// 	*argc = get_args_cnt(tmp_token);
+// 	argv = malloc(sizeof(char *) * (*argc + 1));
+// 	if (argv == NULL)
+// 		fatal_error("malloc error");
+// 	i = 0;
+// 	while (tmp_token->word != NULL)
+// 	{
+// 		// tmp_token->wordをdupして、argvをに代入
+// 		argv[i] = ft_strdup(tmp_token->word);
+// 		tmp_token = tmp_token->next;
+// 		i++;
+// 	}
+// 	argv[i] = NULL;
+// 	return (argv);
+// }
 
 int	search_bi_cmd(t_node *node, t_environ *environ)
 {
 	int		flag;
 	char	**argv;
-	int		argc;
 
 	flag = 0;
-	argv = string_to_array(&argc, node->args);
+	// argv = string_to_array(&argc, node->args);
+	argv = token_list_to_array(node->args);
 	if (argv == NULL)
 		return (0);
 	if (check_bi(argv[0]))
@@ -115,7 +114,7 @@ int	search_bi_cmd(t_node *node, t_environ *environ)
 			free_argv(argv);
 			return (-1);
 		}
-		flag = search_bi_cmd_helper(argc, argv, environ);
+		flag = search_bi_cmd_helper((int)get_token_count(node->args), argv, environ);
 		reset_redirect(node->redirects);
 	}
 	else
