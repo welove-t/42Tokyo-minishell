24ac74b4bd7762ec3aebd4741fa57e50cd19c247
love <tlove.tky@gmail.com>
2023-04-29 15:28:33 +0900

    fix free-token 再帰で後続のノードからfreeするように変更

--- srcs/destructors.c
+++ srcs/destructors.c
@@ -6,7 +6,7 @@
 /*   By: terabu <terabu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 11:56:02 by terabu            #+#    #+#             */
-/*   Updated: 2023/04/29 12:15:32 by terabu           ###   ########.fr       */
+/*   Updated: 2023/04/29 15:26:32 by terabu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 void	free_token(t_token *tok)
 {
-	while (tok != NULL)
-	{
+	if (tok)
+		free_token(tok->next);
+	else
+		return ;
+	if (tok->word)
 		free(tok->word);
-		free(tok);
-		tok = tok->next;
-	}
+	free(tok);
 }
+
