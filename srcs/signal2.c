cfc501c033d2424153c3528ef1c5e8af968208f6
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-05-01 14:45:34 +0900

    fix: 不要なシグナル関連の関数を削除

--- srcs/signal2.c
+++ srcs/signal2.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   signal2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: susasaki <susasaki@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 15:12:34 by susasaki          #+#    #+#             */
-/*   Updated: 2023/04/30 15:28:42 by susasaki         ###   ########.fr       */
+/*   Updated: 2023/05/01 14:44:12 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	signal_handler_waiting_input(int sig)
-{
-	printf("test\n");
-	printf("sig = %d\n", sig);
-	if (sig == SIGINT)
-	{
-		signal(SIGINT, SIG_DFL);
-		g_global.status = 130;
-		signal(SIGINT, signal_handler_waiting_input);
-	}
-	else if (sig == SIGQUIT)
-	{
-		signal(SIGQUIT, SIG_DFL);
-		g_global.status = 131;
-		signal(SIGQUIT, signal_handler_waiting_input);
-	}
-}
+// void	signal_handler_waiting_input(int sig)
+// {
+// 	printf("test\n");
+// 	printf("sig = %d\n", sig);
+// 	if (sig == SIGINT)
+// 	{
+// 		signal(SIGINT, SIG_DFL);
+// 		g_global.status = 130;
+// 		signal(SIGINT, signal_handler_waiting_input);
+// 	}
+// 	else if (sig == SIGQUIT)
+// 	{
+// 		signal(SIGQUIT, SIG_DFL);
+// 		g_global.status = 131;
+// 		signal(SIGQUIT, signal_handler_waiting_input);
+// 	}
+// }
