827e0b791c01548f1165470742968ba5baa274bd
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-05-01 15:31:31 +0900

    fix: ./a.out 無限ループするプログラムをctrl-c押した時に終了するようにした

--- srcs/utils/exec.c
+++ srcs/utils/exec.c
@@ -6,7 +6,7 @@
 /*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 13:38:03 by terabu            #+#    #+#             */
-/*   Updated: 2023/05/01 14:44:29 by susasaki         ###   ########.fr       */
+/*   Updated: 2023/05/01 15:30:21 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,10 @@ void	exec_cmd(t_node *node)
 	{
 		if (cmd_line[0] != NULL)
 		{
+			//ctrl-c: 130
+			signal(SIGINT, signal_handler_waiting_input);
+			//ctrl-\: 131
+			signal(SIGQUIT, signal_handler_waiting_input);
 			if (execve(cmd_line[0], cmd_line, environ) == -1)
 				fatal_error("execv");
 		}
