f62c32d5c1213ebeca534d261d107d7d54f13ac3
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-05-03 15:58:46 +0900

    fix: プロンプトに"/"が入力された終了ステータスをbashの挙動に合わせ、126に設定

--- srcs/error_cmd.c
+++ srcs/error_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_cmd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: terabu <terabu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 12:58:24 by terabu            #+#    #+#             */
-/*   Updated: 2023/05/01 14:48:12 by terabu           ###   ########.fr       */
+/*   Updated: 2023/05/03 15:58:05 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 void	error_cmd(char *cmd)
 {
 	perror_prefix();
+	if (ft_strlen(cmd) == 1 && cmd[0] == '/')
+	{
+		put_error_msg(cmd);
+		put_error_msg_endl(": No such file or directory");
+		exit (126);
+	}
 	if (ft_strchr(cmd, '/'))
 	{
 		put_error_msg(cmd);
